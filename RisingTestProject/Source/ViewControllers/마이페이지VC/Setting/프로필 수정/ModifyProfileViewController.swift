e4d17a963b6a4595384619f6c0f0db40c368d473
DongHee-Sin <voslr0601@naver.com>
2022-03-22 10:31:03 +0900

    1. README 업데이트 2. 프로필 수정 VC 작업중

---
+++ RisingTestProject/Source/ViewControllers/마이페이지VC/Setting/프로필 수정/ModifyProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ModifyProfileViewController.swift
+//  RisingTestProject
+//
+//  Created by 신동희 on 2022/03/22.
+//
+
+import UIKit
+
+class ModifyProfileViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
