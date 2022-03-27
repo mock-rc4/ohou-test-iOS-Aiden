fdfa327ba97e105dac085e69e84caf0bb0caa5a4
DongHee-Sin <voslr0601@naver.com>
2022-03-27 10:20:16 +0900

    결제VC) 결제수단Cell 생성 -> 라디오 버튼으로 작동

--- RisingTestProject/Source/ViewControllers/스토어 VC/결제하기 VC/PaymentViewController.swift
+++ RisingTestProject/Source/ViewControllers/스토어 VC/결제하기 VC/PaymentViewController.swift
@@ -30,6 +30,7 @@ class PaymentViewController: BaseViewController {
         tableView.register(UINib(nibName: "DeliveryAddressTableViewCell", bundle: nil), forCellReuseIdentifier: "DeliveryAddressTableViewCell")
         tableView.register(UINib(nibName: "OrdererTableViewCell", bundle: nil), forCellReuseIdentifier: "OrdererTableViewCell")
         tableView.register(UINib(nibName: "OrderProductTableViewCell", bundle: nil), forCellReuseIdentifier: "OrderProductTableViewCell")
+        tableView.register(UINib(nibName: "PaymentMethodTableViewCell", bundle: nil), forCellReuseIdentifier: "PaymentMethodTableViewCell")
         
         // footer
 //        tableView.register(UINib(nibName: <#T##String#>, bundle: <#T##Bundle?#>), forHeaderFooterViewReuseIdentifier: <#T##String#>)
@@ -81,7 +82,7 @@ extension PaymentViewController: UITableViewDelegate, UITableViewDataSource {
         }
         // 결제수단 cell
         else if indexPath.section == 3 {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath) as? DeliveryAddressTableViewCell else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "PaymentMethodTableViewCell", for: indexPath) as? PaymentMethodTableViewCell else {
                 return UITableViewCell()
             }
             addSeparator(cell)
@@ -108,7 +109,7 @@ extension PaymentViewController: UITableViewDelegate, UITableViewDataSource {
     
     // 섹션의 개수
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return 4
     }
     
     
@@ -117,7 +118,7 @@ extension PaymentViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         // 배송지 cell
         if indexPath.section == 0 {
-            return tableView.frame.width / 1.5
+            return tableView.frame.width / 1.4
         }
         // 주문자 cell
         else if indexPath.section == 1 {
@@ -129,7 +130,7 @@ extension PaymentViewController: UITableViewDelegate, UITableViewDataSource {
         }
         // 결제수단 cell
         else if indexPath.section == 3 {
-            return tableView.frame.width / 2
+            return tableView.frame.width * 0.73
         }
         // 결제금액 cell
         else if indexPath.section == 4 {
--- RisingTestProject/Source/ViewControllers/스토어 VC/결제하기 VC/TableCell/결제수단 Cell/PaymentMethodTableViewCell.swift
+++ RisingTestProject/Source/ViewControllers/스토어 VC/결제하기 VC/TableCell/결제수단 Cell/PaymentMethodTableViewCell.swift
@@ -9,9 +9,100 @@ import UIKit
 
 class PaymentMethodTableViewCell: UITableViewCell {
 
+    @IBOutlet var buttomGroub: [UIButton]!
+    
+    var selectedView: UIView?
+    
+    @IBOutlet weak var cardView: UIView!
+    @IBAction func didTapCardButton(_ sender: UIButton) {
+        if let view = selectedView {
+            deselected(view)
+        }
+        selected(cardView)
+        selectedView = cardView
+    }
+    
+    
+    @IBOutlet weak var naverView: UIView!
+    @IBAction func didTapNaverButton(_ sender: Any) {
+        if let view = selectedView {
+            deselected(view)
+        }
+        selected(naverView)
+        selectedView = naverView
+    }
+    
+    
+    @IBOutlet weak var creditView: UIView!
+    @IBAction func didTapCreditButton(_ sender: Any) {
+        if let view = selectedView {
+            deselected(view)
+        }
+        selected(creditView)
+        selectedView = creditView
+    }
+    
+    
+    
+    @IBOutlet weak var paycoView: UIView!
+    @IBAction func didTapPaycoButton(_ sender: Any) {
+        if let view = selectedView {
+            deselected(view)
+        }
+        selected(paycoView)
+        selectedView = paycoView
+    }
+    
+    
+    
+    @IBOutlet weak var kakaoView: UIView!
+    @IBAction func didTapKakaoButton(_ sender: Any) {
+        if let view = selectedView {
+            deselected(view)
+        }
+        selected(kakaoView)
+        selectedView = kakaoView
+    }
+    
+    
+    @IBOutlet weak var chaiView: UIView!
+    @IBAction func didTapChaiButton(_ sender: Any) {
+        if let view = selectedView {
+            deselected(view)
+        }
+        selected(chaiView)
+        selectedView = chaiView
+    }
+    
+    
+    @IBOutlet weak var tossView: UIView!
+    @IBAction func didTapTossButton(_ sender: Any) {
+        if let view = selectedView {
+            deselected(view)
+        }
+        selected(tossView)
+        selectedView = tossView
+    }
+    
+    
+    
+    @IBOutlet weak var phoneView: UIView!
+    @IBAction func didTapPhoneButton(_ sender: Any) {
+        if let view = selectedView {
+            deselected(view)
+        }
+        selected(phoneView)
+        selectedView = phoneView
+    }
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        buttomGroub.forEach({
+            deselected($0)
+        })
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -20,4 +111,13 @@ class PaymentMethodTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func selected(_ view: UIView) {
+        view.layer.borderWidth = 2
+        view.layer.borderColor = UIColor.systemCyan.cgColor
+    }
+    
+    func deselected(_ view: UIView) {
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+    }
 }
